a80eb7cab481512e36f229088ecb7022cdd27c69
“Your <>
2023-07-02 12:24:19 +0700

    update CORs

--- server/server.js
+++ server/server.js
@@ -3,8 +3,15 @@ require("dotenv").config();
 const dbConnect = require("./config/dbconnect");
 const initRouter = require("./routes");
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
 const app = express();
+app.use(
+  cors({
+    origin: process.env.URL_CLIENT,
+    methods: ["POST", "PATCH", "GET", "DELETE"],
+  })
+);
 app.use(cookieParser());
 const port = process.env.PORT || 8888;
 app.use(express.json());
